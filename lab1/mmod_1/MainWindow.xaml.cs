22a2066676d4f2eed5e24bfaaac6159673f03faf
bumblebee <tim.obozny@gmail.com>
2023-09-24 22:00:44 +0300

    Created GUI without any functionality

---
+++ lab1/mmod_1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+
+namespace mmod_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mySharedOwnWindow.window = this;
+        }
+
+        private void btn_task1_Click(object sender, RoutedEventArgs e)
+        {
+            Task1 task1 = new Task1();
+            task1.Show();
+            this.Hide();
+        }
+
+        private void btn_task2_Click(object sender, RoutedEventArgs e)
+        {
+            Task2 task2 = new Task2();
+            task2.Show();
+            this.Hide();
+        }
+
+        private void btn_task3_Click(object sender, RoutedEventArgs e)
+        {
+            Task3 task3 = new Task3();
+            task3.Show();
+            this.Hide();
+        }
+
+        private void btn_task4_Click(object sender, RoutedEventArgs e)
+        {
+            Task4 task4 = new Task4();
+            task4.Show();
+            this.Hide();
+        }
+
+        private void myOwnWindow_Closed(object sender, System.EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+    public static class mySharedOwnWindow
+    {
+        public static MainWindow window = new MainWindow();
+    }
+}
