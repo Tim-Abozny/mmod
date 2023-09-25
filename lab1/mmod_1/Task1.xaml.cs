3302ee2bc7b3dd064f17162b2a1386df9fb3e103
bumblebee <tim.obozny@gmail.com>
2023-09-25 13:32:03 +0300

    Solved 1 task | Created better UI

--- lab1/mmod_1/Task1.xaml.cs
+++ lab1/mmod_1/Task1.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace mmod_1
 {
@@ -19,6 +11,39 @@ namespace mmod_1
     /// </summary>
     public partial class Task1 : Window
     {
+        public static class RandomNumberGenerator
+        {
+            public static Random random = new Random();
+            public static double GenerateEvent(double probability, int N, ListBox answersList)
+            {
+                List<string> myList = new List<string>();
+
+                int eventOccurred = 0;
+                double tempDouble;
+                for (int i = 0; i < N; i++)
+                {
+                    tempDouble = random.NextDouble();
+                    if (tempDouble <= probability)
+                    {
+                        eventOccurred++;
+                        myList.Add(tempDouble.ToString() + "\t\t" + "True");
+                    }
+                    else
+                    {
+                        myList.Add(tempDouble.ToString() + "\t\t" + "False");
+                    }
+                }
+                myList.Add("How many True values:\t" + eventOccurred.ToString());
+                double empiricalProbability = (double)eventOccurred / N;
+                foreach (var item in myList)
+                {
+                    answersList.Items.Add(item);
+                }
+                myList = null;
+                return empiricalProbability;
+            }
+        }
+
         public Task1()
         {
             InitializeComponent();
@@ -29,5 +54,42 @@ namespace mmod_1
             mySharedOwnWindow.window.Show();
             this.Close();
         }
+
+        private void btn_ready_Click(object sender, RoutedEventArgs e)
+        {
+            answersList.Items.Clear();
+            double probability = Double.Parse(probabilityTextBox.Text);
+            int N = (int)Math.Pow(10, 6);
+            double empiricalProbability = RandomNumberGenerator.GenerateEvent(probability, N, answersList);
+            probabilityRealTextBox.Text = empiricalProbability.ToString();
+            answersList.Items.Add("Здесь могла быть ваша реклама");
+        }
     }
 }
+#region Пояснение к лабе
+    /*Суть метода Монте-Карло для оценки вероятности события:
+
+    Сгенерировать N случайных чисел равномерно распределенных 
+    в интервале [0, 1]. Это можно сделать, например, с помощью 
+    генератора случайных чисел.
+
+    Для каждого сгенерированного числа проверить, меньше ли оно 
+    заданной вероятности (порога). Если сгенерированное число меньше
+    вероятности, то считать, что событие произошло (True); в
+    противном случае, считать, что событие не произошло (False).
+
+    Подсчитать количество True и False среди сгенерированных значений.
+
+    Оценить частоту выпадения события, поделив количество True
+    на общее количество сгенерированных значений.
+
+    Теоретический расчет вероятности события:
+
+    Если вероятность события равна P, то теоретически вероятность
+    события произойти равна P. Таким образом, теоретическая вероятность 
+    события равна заданной вероятности P.
+
+    Итак, для каждого сгенерированного числа, если оно меньше или равно P,
+    то возвращаем True, иначе возвращаем False. После генерации N = 10^6 значений,
+    подсчитываем количество True и False и сравниваем с теоретическим значением P.*/
+#endregion
--- lab1/mmod_1/Task2.xaml.cs
+++ lab1/mmod_1/Task2.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace mmod_1
 {
@@ -22,6 +11,12 @@ namespace mmod_1
         public Task2()
         {
             InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            mySharedOwnWindow.window.Show();
+            this.Close();
         }
     }
 }
